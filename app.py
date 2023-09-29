91c2a7df2c37a56b827ddf8695cfc4b37ddf585e
Swaroop Ajit Palayat <52479733+swaroopajit@users.noreply.github.com>
2023-09-29 10:06:46 +0530

    updated kwen file

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify, Response
 from flask_cors import CORS,cross_origin
 import torch
-from transformers import AutoProcessor, AutoModelForCausalLM
 import numpy as np
 import base64
 import os
@@ -14,26 +13,25 @@ from pathlib import Path
 
 import copy
 import os
-from modelscope import (
+import re
+import secrets
+import tempfile
+from argparse import ArgumentParser
+from pathlib import Path
+from transformers import (
     AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 )
 from huggingface_hub import snapshot_download
+
 
 DEFAULT_CKPT_PATH = '4bit/Qwen-VL-Chat-Int4'
 REVISION = 'v1.0.0'
 BOX_TAG_PATTERN = r"<box>([\s\S]*?)</box>"
 PUNCTUATION = "！？。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏."
 
-#make shots directory to save pics
-try:
-    os.mkdir('./shots')
-except OSError as error:
-    pass
-
 app = Flask(__name__)
 CORS(app)
 
-# MARK :- don't change this
 def _get_args():
     parser = ArgumentParser()
     parser.add_argument("-c", "--checkpoint-path", type=str, default=DEFAULT_CKPT_PATH,
@@ -53,7 +51,6 @@ def _get_args():
     args = parser.parse_args()
     return args
 
-# MARK :- don't change this
 def _load_model_tokenizer(args):
     model_id = args.checkpoint_path
     model_dir = snapshot_download(model_id)
@@ -111,38 +108,13 @@ def _parse_text(text):
     text = "".join(lines)
     return text
 
-args = _get_args()
+def predict(model, tokenizer, image_b64, query):
+    print("User: " + query)
+    history = [query]  # Assuming you want to include the query in the conversation history
+    message = image_b64  # Assuming image_b64 contains the base64 encoded image
 
-model, tokenizer = _load_model_tokenizer(args)
-
-def predict():
-    print('predict called')
-    
-    query = 'describe the outfit'
-    print("User: " + _parse_text(query))
-    full_response = ""
-
-    history_filter = []
-    pic_idx = 1
-    pre = ""
-    if isinstance(q, (tuple, list)):
-        q = f'Picture {pic_idx}: <img>{q[0]}</img>'
-        pre += q + '\n'
-        pic_idx += 1
-    else:
-        pre += q
-        history_filter.append((pre, a))
-        pre = ""
-    history, message = history_filter[:-1], history_filter[-1][0]
-    response, history = model.chat(tokenizer, message, history=history)
-    image = tokenizer.draw_bbox_on_latest_picture(response, history)
-    if image is None:
-        _chatbot[-1] = (_parse_text(chat_query), response)
-    full_response = _parse_text(response)
-
-    task_history[-1] = (query, full_response)
-    print("Qwen-VL-Chat: " + _parse_text(full_response))
-    task_history = task_history[-10:]
+    response, _ = model.chat(tokenizer, message, history=history)
+    # Process the response if needed
     return response
 
 def add_file(history, task_history, file):
@@ -162,38 +134,46 @@ def reset_state(task_history):
 def process_image():
     start_time = time.time()  # Record the start time
 
-    # Get the base64-encoded image from the request JSON
+    # # Get the base64-encoded image from the request JSON
     request_data = request.get_json()
     image_data = request_data['base64_image']
 
-    # Decode the base64 string
-    image_bytes = base64.b64decode(image_data)
+    # # Decode the base64 string
+    # image_bytes = base64.b64decode(image_data)
 
-    # Create a PIL image object
-    img = Image.open(BytesIO(image_bytes))
+    # # Create a PIL image object
+    # img = Image.open(BytesIO(image_bytes))
 
-    # Process the image (e.g., apply filters, resize, etc.)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # # Process the image (e.g., apply filters, resize, etc.)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Convert image to a numpy array
-    image_array = np.array(img)
+    # # Convert image to a numpy array
+    # image_array = np.array(img)
 
-    # Convert numpy array to a tensor
-    image = torch.tensor(image_array, dtype=torch.float32).permute(2, 0, 1) / 255.0
-    image = image.unsqueeze(0).to(device)
+    # # Convert numpy array to a tensor
+    # image = torch.tensor(image_array, dtype=torch.float32).permute(2, 0, 1) / 255.0
+    # image = image.unsqueeze(0).to(device)
 
-    # Assuming `model` and `processor` are defined earlier in your code
-    model.to(device)
-    inputs = processor(images=image, return_tensors="pt", do_rescale=False).to(device)
-    pixel_values = inputs.pixel_values
-    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    # # Assuming `model` and `processor` are defined earlier in your code
+    # model.to(device)
+    # inputs = processor(images=image, return_tensors="pt", do_rescale=False).to(device)
+    # pixel_values = inputs.pixel_values
+    # generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    # generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
+    args = _get_args()
+    model, tokenizer = _load_model_tokenizer(args)
+
+    query = 'describe the outfit'
+    generated_caption = predict(model, tokenizer, image_data, query)
+
+    # Process the response if needed
+    
     end_time = time.time()  # Record the end time
     runtime = end_time - start_time  # Calculate the runtime
 
     # Save the processed image (replace 'p' with the desired file path)
-    img.save('processed_image.jpg')
+    # img.save('processed_image.jpg')
 
     # Return the processed image and caption
     return jsonify({
