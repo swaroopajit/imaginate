a4442a2c6b7e36b3dca2dec3acceec79161b7484
Swaroop Ajit Palayat <52479733+swaroopajit@users.noreply.github.com>
2023-09-29 11:29:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, jsonify, Response
+from flask_cors import CORS,cross_origin
 import torch
-from transformers import AutoProcessor, AutoModelForCausalLM
 import numpy as np
 import base64
 import os
 import time
 from PIL import Image
 from io import BytesIO
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation import GenerationConfig
+import torch
+import datetime
+torch.manual_seed(1234)
 
 #make shots directory to save pics
 try:
@@ -14,21 +19,22 @@ try:
 except OSError as error:
     pass
 
-global capture
-capture=0
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL", device_map="cuda", trust_remote_code=True).eval()
 
 app = Flask(__name__)
-
-# Initialize the processor and model
-processor = AutoProcessor.from_pretrained("swaroopajit/git-base-next-refined")
-model = AutoModelForCausalLM.from_pretrained("swaroopajit/git-base-next-refined")
-
-app = Flask(__name__)
+CORS(app)
 
 # Define a route that accepts POST requests
 @app.route('/get_caption', methods=['POST'])
+@cross_origin()
 def process_image():
     start_time = time.time()  # Record the start time
+
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    
+    # Create the filename with datetime stamp
+    filename = f"{current_datetime}.jpg"
 
     # Get the base64-encoded image from the request JSON
     request_data = request.get_json()
@@ -40,28 +46,40 @@ def process_image():
     # Create a PIL image object
     img = Image.open(BytesIO(image_bytes))
 
-    # Process the image (e.g., apply filters, resize, etc.)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Save the processed image (replace 'p' with the desired file path)
+    img.save(filename)
 
     # Convert image to a numpy array
-    image_array = np.array(img)
+    # image_array = np.array(img)
 
-    # Convert numpy array to a tensor
-    image = torch.tensor(image_array, dtype=torch.float32).permute(2, 0, 1) / 255.0
-    image = image.unsqueeze(0).to(device)
+    # # Convert numpy array to a tensor
+    # image = torch.tensor(image_array, dtype=torch.float32).permute(2, 0, 1) / 255.0
+    # image = image.unsqueeze(0).to(device)
 
-    # Assuming `model` and `processor` are defined earlier in your code
-    model.to(device)
-    inputs = processor(images=image, return_tensors="pt", do_rescale=False).to(device)
-    pixel_values = inputs.pixel_values
-    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    query = tokenizer.from_list_format([
+        {'image': filename},
+        {'text': 'Describe the outfit'},
+    ])
+    inputs = tokenizer(query, return_tensors='pt')
+    inputs = inputs.to(model.device)
+    pred = model.generate(**inputs)
+    generated_caption = tokenizer.decode(pred.cpu()[0], skip_special_tokens=False)
+
+    # Process the image (e.g., apply filters, resize, etc.)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    # # Assuming `model` and `processor` are defined earlier in your code
+    # model.to(device)
+    # inputs = processor(images=image, return_tensors="pt", do_rescale=False).to(device)
+    # pixel_values = inputs.pixel_values
+    # generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    # generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
     end_time = time.time()  # Record the end time
     runtime = end_time - start_time  # Calculate the runtime
 
-    # Save the processed image (replace 'p' with the desired file path)
-    img.save('processed_image.jpg')
 
     # Return the processed image and caption
     return jsonify({
@@ -72,63 +90,6 @@ def process_image():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# def generate_frames():
-#     camera = cv2.VideoCapture(0)
-#     global capture
-#     while True:
-#         success, frame = camera.read()
-#         if not success:
-#             break
-#         else:
-#             if(capture):
-#                 # do the infrencing here hopefully
-#                 start_time = time.time()  # Record the start time
-#                 capture=0
-#                 now = datetime.datetime.now()
-#                 p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
-#                 cv2.imwrite(p, frame)
-
-#                 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-                
-#                 # Convert the image to tensor
-#                 image = torch.tensor(frame, dtype=torch.float32).permute(2, 0, 1) / 255.0
-
-#                 model.to(device)  # Ensure the model is on the same device as the input
-#                 inputs = processor(images=image, return_tensors="pt", do_rescale=False).to(device)
-#                 pixel_values = inputs.pixel_values
-#                 generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-#                 generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-#                 end_time = time.time()  # Record the end time
-#                 runtime = end_time - start_time  # Calculate the runtime
-
-#                 print(generated_caption)
-#                 print(runtime)
-
-#             try:
-#                 ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-#                 frame = buffer.tobytes()
-#                 yield (b'--frame\r\n'
-#                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#             except Exception as e:
-#                 pass
-#     camera.release()
-#     cv2.destroyAllWindows()
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# @app.route('/capture',methods=['POST'])
-# def capture():
-#     global capture
-#     capture=1
-#     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
