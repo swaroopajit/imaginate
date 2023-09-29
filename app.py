fc4f3dee0add4b1f0bd06cd60d02841b8f702c67
Swaroop Ajit Palayat <52479733+swaroopajit@users.noreply.github.com>
2023-09-29 17:42:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
 import torch
 import datetime
+import csv
 torch.manual_seed(1234)
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
@@ -21,9 +22,21 @@ try:
 except OSError as error:
     pass
 
-gender_list = {}
-type_list = {}
-color_list = {}
+def read_csv(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
+
+def find_string(input_string, word_list):
+    for word in word_list:
+        if word in input_string:
+            return word
+    return ""
+
+gender_list = read_csv('gender.csv')
+type_list = read_csv('categories.csv')
+color_list = read_csv('colors.csv')
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat-Int4", trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat-Int4", device_map="auto", trust_remote_code=True).eval()
@@ -56,6 +69,12 @@ def process_image():
     img.save(filename)
 
     response, history = model.chat(tokenizer, query = f'<img>{filename}</img>Describe the outfit', history = None)
+
+    #
+
+    modal_name = find_string(response, type_list)
+    color = find_string(response, color_list)
+    gender = find_string(response, gender_list)
 
     #color, history = model.chat(tokenizer, query = f'<img>{filename}</img>Mention the color of the outfit', history = None)
 
